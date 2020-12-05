bd74953c6ff5be49cb0507e10510561394002c65
tommyamg18 <50183786+tommyamg18@users.noreply.github.com>
2020-12-05 16:51:46 -0400

    Agregacion de confirmacion Cliente 1.0.11.1

---
+++ ProyectoFinalP1_8G2/src/visual/ConfirmacionCliente.java
@@ -0,0 +1,148 @@
+package visual;
+
+import java.awt.BorderLayout;
+
+
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JFormattedTextField;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.TitledBorder;
+import javax.swing.text.MaskFormatter;
+
+import logic.Altice;
+import logic.Cliente;
+
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+import javax.swing.JFormattedTextField.AbstractFormatter;
+import javax.swing.JRadioButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class ConfirmacionCliente extends JDialog {
+
+	/**
+	 * 
+	 */
+	private final JPanel contentPanel = new JPanel();
+	private JTextField textField;
+	private JRadioButton rdbtnExistente;
+	private JRadioButton rdbtnNuevo;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		try {
+			ConfirmacionCliente dialog = new ConfirmacionCliente();
+			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+			dialog.setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Create the dialog.
+	 */
+	public ConfirmacionCliente() {
+		setResizable(false);
+		setTitle("Opciones de Clientes");
+		setBounds(100, 100, 492, 328);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBorder(new TitledBorder(null, "Opciones de Clientes", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		panel.setBounds(11, 0, 460, 221);
+		contentPanel.add(panel);
+		panel.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Seleccionar tipo de Cliente");
+		lblNewLabel.setBounds(53, 46, 338, 33);
+		panel.add(lblNewLabel);
+		
+		textField = new JFormattedTextField((AbstractFormatter) null);
+		try {
+			MaskFormatter formatoCedula= new MaskFormatter("###-#######-#");
+			formatoCedula.setPlaceholderCharacter('_');
+			textField = new JFormattedTextField(formatoCedula);			
+		}catch (Exception e) {
+			// TODO: handle exception
+		}
+		textField.setEditable(false);
+		textField.setBounds(104, 159, 236, 39);
+		panel.add(textField);
+		textField.setColumns(10);
+		
+		rdbtnNuevo = new JRadioButton("Nuevo");
+		rdbtnNuevo.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				rdbtnNuevo.setSelected(true);
+				rdbtnExistente.setSelected(false);
+				textField.setEditable(false);
+			}
+		});
+		rdbtnNuevo.setSelected(true);
+		rdbtnNuevo.setBounds(271, 90, 167, 41);
+		panel.add(rdbtnNuevo);
+		
+		rdbtnExistente = new JRadioButton("Existente");
+		rdbtnExistente.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				rdbtnNuevo.setSelected(false);
+				rdbtnExistente.setSelected(true);
+				textField.setEditable(true);
+			}
+		});
+		rdbtnExistente.setBounds(63, 90, 184, 41);
+		panel.add(rdbtnExistente);
+		{
+			JPanel buttonPane = new JPanel();
+			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+			getContentPane().add(buttonPane, BorderLayout.SOUTH);
+			{
+				JButton okButton = new JButton("Confirmar");
+				okButton.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent arg0) {
+						if(rdbtnExistente.isSelected()) {
+							Cliente miCliente = Altice.getInstance().buscarCliente(textField.getText());
+							if(miCliente!= null) {
+							Facturacion newFact = new Facturacion(0,miCliente);
+							newFact.setVisible(true);
+							dispose();
+							}else {
+								JOptionPane.showMessageDialog(null, "Cedula No encontrada, \nPor favor Verificar", "Error", JOptionPane.ERROR_MESSAGE);
+								textField.setText("");
+							}
+						}else if(rdbtnNuevo.isSelected()) {
+							RegCliente newClient = new RegCliente(0, null);
+							newClient.setVisible(true);
+							dispose();
+						}
+					}
+				});
+				okButton.setActionCommand("ok");
+				buttonPane.add(okButton);
+				getRootPane().setDefaultButton(okButton);
+			}
+			{
+				JButton cancelButton = new JButton("Cancel");
+				cancelButton.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						dispose();
+					}
+				});
+				cancelButton.setActionCommand("Cancel");
+				buttonPane.add(cancelButton);
+			}
+		}
+	}
+}
